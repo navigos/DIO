8d918b9e54879f9b04b490bf05ceacaed3a5a2f3
navigos <navigos@proton.me>
2023-04-02 18:56:38 -0300

    IaC Files v.2

--- bash_script.sh
+++ bash_script.sh
@@ -15,21 +15,30 @@ chown root:GRP_ADM /adm & chown root:GRP_VEN /ven & chown root:GRP_SEC /sec
 
 echo "Criando usuários."
 useradd carlos -c "Carlos" -s /bin/bash -p $(openssl passwd -e -1 Senha123) -m -G GRP_ADM
+passwd carlos -e
 
 useradd maria -c "Maria" -s /bin/bash -p $(openssl passwd -1 Senha123) -m -G GRP_ADM
+passwd maria -e
 
 useradd joao -c "João" -s /bin/bash -p $(openssl passwd -1 Senha123) -m -G GRP_ADM
+passwd joao -e
 
 useradd debora -c "Debora" -s /bin/bash -p $(openssl passwd -1 Senha123) -m -G GRP_VEN
+passwd debora -e
 
 useradd sebastiana -c "Sebastiana" -s /bin/bash -p $(openssl passwd -1 Senha123) -m -G GRP_VEN
+passwd sebastiana -e
 
 useradd roberto -c "Roberto" -s /bin/bash -p $(openssl passwd -1 Senha123) -m -G GRP_VEN
+passwd roberto -e
 
 useradd josefina -c "Josefina" -s /bin/bash -p $(openssl passwd -1 Senha123) -m -G GRP_SEC
+passwd josefina -e
 
 useradd amanda -c "Amanda" -s /bin/bash -p $(openssl passwd -1 Senha123) -m -G GRP_SEC
+passwd amanda -e
 
 useradd rogerio -c "Rogerio" -s /bin/bash -p $(openssl passwd -1 Senha123) -m -G GRP_SEC
+passwd rogerio -e
 
 echo "Finalizado!!!"
